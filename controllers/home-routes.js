cd1cbbe5dd4fef79ee51febb1b4d3c438cc5c610
Ricky Bobby <richard.pajewski@gmail.com>
2021-01-03 13:59:56 -0600

    stuff

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -26,7 +26,11 @@ router.get('/', (req, res) => {
             randomPlantsArray.push(randomPlants);
         }
         const plants = randomPlantsArray.flat(1)
-        res.render('homepage', { plants, data, loggedIn: req.session.loggedIn })
+        res.render('homepage', {
+            plants,
+            data,
+            loggedIn: req.session.loggedIn
+        })
     })
     .catch(err => {
         console.log(err);
